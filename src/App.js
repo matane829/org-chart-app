3fe1af6051d091ff25090de52434ea7765eb5c93
Matan Ezra <matanezra28@gmail.com>
2024-05-05 10:57:48 +0300

    version 2.4.2

--- src/App.js
+++ src/App.js
@@ -3,24 +3,21 @@ import './index.css';
 import { OrgChartComponent } from './components/OrgChart';
 
 function App() {
-const [data, setData] = React.useState([
-{'id': 'mahlaka first', 'parentId': null, 'level': 'mahlaka'},
-{'id': 'first', 'parentId': 'mahlaka first', 'level': 'class'},
-{'id': 'subfirst', 'parentId': 'first', 'level': 'subclass'},
-{'id': 'subsubfirst', 'parentId': 'subfirst', 'level': 'subsubclass'},
-{'id': 'subsubsecond', 'parentId': 'subfirst', 'level': 'subsubclass'},
-{'id': 'subsecond', 'parentId': 'first', 'level': 'subclass'},
-{'id': 'second', 'parentId': 'mahlaka first', 'level': 'class'},
-{'id': 'subthird', 'parentId': 'second', 'level': 'subclass'},
-{'id': 'subfourth', 'parentId': 'second', 'level': 'subclass'},
-]);
+  const [data, setData] = React.useState([
+    {'id': 'mahlaka first', 'parentId': null, 'level': 'mahlaka'},
+    {'id': 'first', 'parentId': 'mahlaka first', 'level': 'class'},
+    {'id': 'subfirst', 'parentId': 'first', 'level': 'subclass'},
+    {'id': 'subsubfirst', 'parentId': 'subfirst', 'level': 'subsubclass'},
+    {'id': 'subsubsecond', 'parentId': 'subfirst', 'level': 'subsubclass'},
+    {'id': 'subsecond', 'parentId': 'first', 'level': 'subclass'},
+    {'id': 'second', 'parentId': 'mahlaka first', 'level': 'class'},
+    {'id': 'subthird', 'parentId': 'second', 'level': 'subclass'},
+    {'id': 'subfourth', 'parentId': 'second', 'level': 'subclass'},
+  ]);
 
   return (
     <div className="App">
-      <OrgChartComponent
-        data={data}
-        setData={data => setData(data)}
-      />
+      <OrgChartComponent data={data} setData={data => setData(data)} />
     </div>
   );
 }
